7167398b7512f57f49bc31f071386e890d0f80ea
DarthDoggys <46267430+DarthDoggys@users.noreply.github.com>
2019-01-07 17:26:59 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,103 +6,103 @@ import time
 from itertools import cycle
 import os
 
-client = commands.Bot(command_prefix = "d")
+bot = commands.Bot(command_prefix = "d")
 status = ["testing the bot", "dhelp"]
 
 async def change_status():
-  await client.wait_until_ready()
+  await bot.wait_until_ready()
   msgs = cycle(status)
   
-  while not client.is_closed:
+  while not bot.is_closed:
     current_status = next(msgs)
-    await client.change_presence(game=discord.Game(name=current_status))
+    await bot.change_presence(game=discord.Game(name=current_status))
     await asyncio.sleep(5)
 
-@client.event
+@bot.event
 async def on_ready():
 	print('Logged in as')
-	print("User name:", client.user.name)
-	print("User id:", client.user.id)
+	print("User name:", bot.user.name)
+	print("User id:", bot.user.id)
 	print('---------------')
 	
 def user_is_me(ctx):
 	return ctx.message.author.id == "277983178914922497"
     
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def ping(ctx):
     """Pings the bot and gets a response time."""
     pingtime = time.time()
-    pingms = await client.say("Pinging...")
+    pingms = await bot.say("Pinging...")
     ping = (time.time() - pingtime) * 1000
-    await client.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
+    await bot.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 @commands.has_permissions(kick_members=True, administrator=True)
 async def mute(ctx, user: discord.Member, *, arg):
 	if arg is None:
-		await client.say("please say a reason to {}".format(user.name))
+		await bot.say("please say a reason to {}".format(user.name))
 		return False
 	reason = arg
 	author = ctx.message.author
 	role = discord.utils.get(ctx.message.server.roles, name="Muted")
-	await client.add_roles(user, role)
+	await bot.add_roles(user, role)
 	embed = discord.Embed(title="Mute", description=" ", color=0xFFA500)
 	embed.add_field(name="User: ", value="<@{}>".format(user.id), inline=False)
 	embed.add_field(name="Moderator: ", value="{}".format(author.mention), inline=False)
 	embed.add_field(name="Reason: ", value="{}\n".format(arg), inline=False)
-	await client.say(embed=embed)
+	await bot.say(embed=embed)
 	
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 @commands.has_permissions(kick_members=True, administrator=True)
 async def unmute(ctx, user: discord.Member, *, arg):
 	if arg is None:
-		await client.say("please say a reason to {}".format(user.name))
+		await bot.say("please say a reason to {}".format(user.name))
 		return False
 	reason = arg
 	author = ctx.message.author
 	role = discord.utils.get(ctx.message.server.roles, name="Muted")
-	await client.remove_roles(user, role)
+	await bot.remove_roles(user, role)
 	embed = discord.Embed(title="Unmute", description=" ", color=0x00ff00)
 	embed.add_field(name="User: ", value="<@{}>".format(user.id), inline=False)
 	embed.add_field(name="Moderator: ", value="{}".format(author.mention), inline=False)
 	embed.add_field(name="Reason: ", value="{}\n".format(arg), inline=False)
-	await client.say(embed=embed)
+	await bot.say(embed=embed)
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, user: discord.Member, *, arg):
 	if arg is None:
-		await client.say("please say a reason to {}".format(user.name))
+		await bot.say("please say a reason to {}".format(user.name))
 		return False
 	reason = arg
 	author = ctx.message.author
-	await client.kick(user)
+	await bot.kick(user)
 	embed = discord.Embed(title="Kick", description=" ", color=0x00ff00)
 	embed.add_field(name="User: ", value="<@{}>".format(user.id), inline=False)
 	embed.add_field(name="Moderator: ", value="{}".format(author.mention), inline=False)
 	embed.add_field(name="Reason: ", value="{}\n".format(arg), inline=False)
-	await client.say(embed=embed)
+	await bot.say(embed=embed)
   
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, user: discord.Member, *, arg):
 	if arg is None:
-		await client.say("please say a reason to {}".format(user.name))
+		await bot.say("please say a reason to {}".format(user.name))
 		return False
 	reason = arg
 	author = ctx.message.author
-	await client.ban(user)
+	await bot.ban(user)
 	embed = discord.Embed(title="Ban", description=" ", color=0xFF0000)
 	embed.add_field(name="User: ", value="<@{}>".format(user.id), inline=False)
 	embed.add_field(name="Moderator: ", value="{}".format(author.mention), inline=False)
 	embed.add_field(name="Reason: ", value="{}\n".format(arg), inline=False)
-	await client.say(embed=embed)
+	await bot.say(embed=embed)
 	
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 @commands.has_permissions(kick_members=True)
 async def warn(ctx, user: discord.Member, *, arg = None):
 	if arg is None:
-		await clieng.say("please say a reason to {}".format(user.name))
+		await bot.say("please say a reason to {}".format(user.name))
 		return False
 	reason = arg
 	author = ctx.message.author
@@ -111,73 +111,73 @@ async def warn(ctx, user: discord.Member, *, arg = None):
 	embed.add_field(name="User: ", value="<@{}>".format(user.id), inline=False)
 	embed.add_field(name="Moderator: ", value="{}".format(author.mention), inline=False)
 	embed.add_field(name="Reason: ", value="{}\n".format(arg), inline=False)
-	await client.say(embed=embed)
-	await client.send_message(user, "You have been warned for: {}".format(reason))
-	await client.send_message(user, "from: {} server".format(server))
+	await bot.say(embed=embed)
+	await bot.send_message(user, "You have been warned for: {}".format(reason))
+	await bot.send_message(user, "from: {} server".format(server))
 	
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 @commands.has_permissions(administrator=True)
 async def addrank(ctx, *, name = None):
 	author = ctx.message.author
 	server = ctx.message.server
 	role = discord.utils.get(ctx.message.server.roles, name=name)
-	await client.create_role(server, name=name)
-	await client.say("the role has been created :thumbsup:")
+	await bot.create_role(server, name=name)
+	await bot.say("the role has been created :thumbsup:")
 	
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 @commands.has_permissions(administrator=True)
 async def delrank(ctx, *, role_name):
   role = discord.utils.get(ctx.message.server.roles, name=role_name)
   if role:
     try:
-      await client.delete_role(ctx.message.server, role)
-      await client.say("The role {} has been deleted!".format(role.name))
+      await bot.delete_role(ctx.message.server, role)
+      await bot.say("The role {} has been deleted!".format(role.name))
     except discord.Forbidden:
-      await client.say("Missing Permissions to delete this role!")
+      await bot.say("Missing Permissions to delete this role!")
   else:
-    await client.say("The role doesn't exist!")
+    await bot.say("The role doesn't exist!")
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 @commands.has_permissions(administrator=True)
 async def addrole(ctx, user: discord.Member = None, *, name = None):
     author = ctx.message.author
     role = discord.utils.get(ctx.message.server.roles, name=name)
-    await client.add_roles(user, role)
-    text = await client.say(f'{author.mention} I have added the {role.name} role to a user {user.name}'.format(role.name))
-    await client.delete_message(ctx.message)
+    await bot.add_roles(user, role)
+    text = await bot.say(f'{author.mention} I have added the {role.name} role to a user {user.name}'.format(role.name))
+    await bot.delete_message(ctx.message)
     await asyncio.sleep(5)
-    await client.delete_message(text)
+    await bot.delete_message(text)
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 @commands.has_permissions(administrator=True)
 async def removerole(ctx, user: discord.Member = None, *, name = None):
     author = ctx.message.author
     role = discord.utils.get(ctx.message.server.roles, name=name)
-    await client.remove_roles(user, role)
-    text = await client.say(f'{author.mention} I have remove the {role.name} role to a user {user.name}'.format(role.name))
-    await client.delete_message(ctx.message)
+    await bot.remove_roles(user, role)
+    text = await bot.say(f'{author.mention} I have remove the {role.name} role to a user {user.name}'.format(role.name))
+    await bot.delete_message(ctx.message)
     await asyncio.sleep(5)
-    await client.delete_message(text)
+    await bot.delete_message(text)
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 @commands.has_permissions(ban_members=True, kick_members=True)
 async def giverole(ctx, user: discord.Member = None, *, name = None):
     author = ctx.message.author
     role = discord.utils.get(ctx.message.server.roles, name=name)
-    await client.add_roles(user, role)
-    text = await client.say(f'{author.mention} I have added the {role.name} role to a user {user.name}'.format(role.name))
-    await client.delete_message(ctx.message)
+    await bot.add_roles(user, role)
+    text = await bot.say(f'{author.mention} I have added the {role.name} role to a user {user.name}'.format(role.name))
+    await bot.delete_message(ctx.message)
     await asyncio.sleep(5)
-    await client.delete_message(text)
-
-@client.command(name='eval', pass_context=True, hidden=True)
+    await bot.delete_message(text)
+	
+@bot.command(name='eval', pass_context=True)
 @commands.check(user_is_me)
 async def _eval(ctx, *, command):
     res = eval(command)
     if inspect.isawaitable(res):
-        await client.say(await res)
+        await bot.say(await res)
     else:
-    	await client.say(res)
+    	await bot.say(res)
 	
-client.loop.create_task(change_status())
-client.run(os.environ['BOT_TOKEN'])
+bot.loop.create_task(change_status())
+bot.run(os.environ['BOT_TOKEN'])
